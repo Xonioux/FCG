8b808965e77ab3a03116c71eede5dcd77042fb97
bedirhanakinn <bedirhanakinn@gmail.com>
2023-01-24 12:20:53 +0100

    button clicks

---
+++ Assets/Sounds/MenuSoundR.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSoundR : MonoBehaviour
+{
+
+   public AudioSource audioR1;
+   public AudioSource audioR2;
+   public AudioSource audioR3;
+   public AudioSource audioR4;
+   public AudioSource audioR5;
+   public AudioSource audioR6;
+   public AudioSource audioR7;
+   public AudioSource audioR8;
+   public AudioSource audioR9;
+
+   public void playButton2()
+   {
+    audioR1.Play();
+    audioR2.Play();
+    audioR3.Play();
+    audioR4.Play();
+    audioR5.Play();
+    audioR6.Play();
+    audioR7.Play();
+    audioR8.Play();
+    audioR9.Play();
+    }
+}
