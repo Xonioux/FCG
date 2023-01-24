1c8509a13be310d871fb5a97ef89ad5add6d5597
Delyan Stoyanov <delyan.m.stoyanov@gmail.com>
2023-01-24 15:06:52 +0100

    Confetti progress
    Added code to trigger confetti
    Still not tested and not sure if it works at all
    after testing and if it works properly they can be added to goal score and other scenes

--- Assets/Scripts/Player/GK Player/Save.cs
+++ Assets/Scripts/Player/GK Player/Save.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class Save : MonoBehaviour
+
+
 {
     GameObject ball;
+    ParticleSystem Confetti;
     public static bool ballSaved;
 
     // Checking with the handcollider if the ball got hit by the hand
@@ -15,7 +18,10 @@ public class Save : MonoBehaviour
         {
             ball = col.gameObject;
             ballSaved = true;
-            
+            void Start()
+             {
+                Confetti = GetComponent<ParticleSystem>();
+             }
             // Creating a little haptic feedback for the controller when the ball gets saved
             GetComponentInParent<XRBaseController>().SendHapticImpulse(.5f, .25f);
             Destroy(col.gameObject, 4f);
